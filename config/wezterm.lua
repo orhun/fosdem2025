38260b4d29d1cd7c0a62a3a337c8b1135e9bd7f1
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2025-01-16 20:23:54 +0300

    Initial setup

---
+++ config/wezterm.lua
@@ -0,0 +1,14 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+config.colors = {
+	background = "#151515",
+	foreground = "#fff",
+}
+config.color_scheme = "Gruvbox dark, pale (base16)"
+config.enable_tab_bar = false
+
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 21.0
+
+return config
---
+++ presentation.md
@@ -0,0 +1,7 @@
+---
+title: Bringing terminal aesthetics to the Web with Rust (and vice versa)
+sub_title: FOSDEM 2025
+author: Orhun Parmaksız
+theme:
+  path: config/theme.yml
+---
